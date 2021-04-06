5fdf00ebe176f791babb7aa10d1c19cd04b39e68
Irene Francisco López <36732712+ifranl00@users.noreply.github.com>
2021-04-06 06:35:34 +0200

    se sube o k

--- p2/gen_rand.c
+++ p2/gen_rand.c
@@ -3,11 +3,8 @@
 #include<string.h>
 #include<time.h>
 
-//const int commands = 4; //number of commands to set the title and plot both int and double random numbers vs sequence numbers (2 plots)
+int find(int array[], int element, int size); // find if there an element in a specific row or 1d array
 
-
-
-int find(int array[], int element, int size); // find if there an element in a specific row
 
 int main(){
 
@@ -17,9 +14,9 @@ int main(){
 
   printf("Insert the number of rows of the sparse matrix:\n"); //ask by console to insert number of rows of matrix to make square matrix of selected dimensions
   scanf("%d", &a); // saved the values entered by console
-  int matrix[a][a];
+  int matrix[a][a]; //matrix for part 1
 
-  /***********  PART 1: fill the main diagonal of the matrix in the range 1-2 and others in range 0-1 ***********/
+  /********************************  PART 1: fill the main diagonal of the matrix in the range 1-2 and others in range 0-1 ********************************/
 
     for (i = 0; i < a; i++)
     {
@@ -108,80 +105,125 @@ int main(){
         printf("\n");
       }
 
-  /*********** PART 3: Record created matrix in CRS (Compressed row storage)***********/
-      FILE *f = fopen("savedMatrix.txt", "w");// Creation of the pointer to the file where the int random numbers are saved in colums
+  /******************************** PART 3: Record created matrix in CRS (Compressed row storage) ********************************/
 
-      if (f == NULL) { // exit the program if an error occured while creating the file
-            printf("Cannot create the file\n"); // message to info
-            exit(1);
-      }
-
-      // fprintf(f, "%d %d \n",axis_x[i],l); //Write the data to the file data.temp
-
-      struct csrFormat {
-        int countRows;
-        int *indexColumn;
-        int *indexRow;
-        int *val;
+      struct csrFormat { // use of a struct which will consist the structure of a sparse matrix in CRS fotmat to record the matrix
+        int *indexColumn; // column indexs of the non-zero elements of the matrix
+        int *indexRow; // locations in val vector that starts a row
+        int *val; // non-zero elements of the matrix
       };
 
-      struct csrFormat m;
-      int c = 0;
+      struct csrFormat m; // create an struct with CRS format called m
+      int c = 0; // to know the index of current non-zero element in the loops below
 
-      m.countRows = a;
-      m.indexColumn = calloc(a*k, sizeof(m.indexColumn[0]));
-      m.indexRow = calloc(a, sizeof(m.indexRow[0]));
-      m.val = calloc(a*k, sizeof(m.val[0]));
+      m.indexColumn = calloc(a*k, sizeof(m.indexColumn[0])); // allocate memory for save the colums which belong to a non-zero element index (num of elements non-zero per row (k) * num of rows (a))
+      m.indexRow = calloc(a, sizeof(m.indexRow[0])); // allocate memory for save the locations in val vector which start a row
+      m.val = calloc(a*k, sizeof(m.val[0])); // allocate memory for save non-zero elements (num of elements non-zero per row (k) * num of rows (a))
 
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i < a; i++) //iterate over rows
+      {
+        for (int j = 0; j < a; j++) //iterate over columns
+        {
+          if(matrix2[i][j]!= 0){ // if the current element is non-zero one
+            m.indexColumn[c] = j+1; // save its column index
+            m.val[c] = matrix2[i][j]; // save the element
+            c++; // increase the counter of elements
+          }
+        }
+        m.indexRow[i] = c; //each value of c when a row is finished saved for store in row_ptr vector of CRS format 
+      }
+
+      // print the sparse matrix in CSR format
+      printf("\n ---- CSR FORMAT -----");
+      printf("\n Val: ");
+
+      for (int i = 0; i < a*k; i++)
+      {
+        printf("%d ",m.val[i]);
+      }
+
+      printf("\n col_in: ");
+
+      for (int i = 0; i < a*k; i++)
+      {
+        printf("%d ",m.indexColumn[i]);
+      }
+
+      printf("\n row_ptr: ");
+
+      for (int i = 0; i < a; i++)
+      {
+        printf("%d ",m.indexRow[i]);
+      }
+
+      /******************************** FINAL PART: Validation of results ********************************/
+    printf("\n");
+
+      // Let's get the standard format of the matrix from the CRS format created before, and check if the matrixs are equal.
+      int matrixFromCRS[a][a]; // save in this variable the matrix generated form CRS format
+      int numNonZeros = 0; // number of non-zero elements
+
+      for (int i = 0; i < a; i++) //iterate over rows
+      {
+        int nonzeroPerRowIndex[k]; // index non-zero elements per row
+        int p = 0; // count of non-zero elements 
+          for(int x = k*i;x<k*i+k;x++){ // to loop from k elements ecah time to obtain the k non-zero values that belongs to current row
+            nonzeroPerRowIndex[p] = m.indexColumn[x]; // get the index non-zero per row from the csr struct parameter named indexColum
+            p++; // increase counter
+          }
+
+          for (int j = 0; j < a; j++) //iterate over columns
+          {
+              
+            if(find(nonzeroPerRowIndex,j+1,k)==1) { // if is a non-zero element to be set
+              matrixFromCRS[i][j] = m.val[numNonZeros]; // assign its value from values of the struct
+              numNonZeros++; // increase counter
+            }else{
+              matrixFromCRS[i][j] = 0; // if its index is not in indexColumn, is a zero value
+            }
+          }
+      }
+
+       //print sparse matrix generated from CRS format
+
+      printf("\nThe sparse matrix generated from CRS format is:\n");
+
+      for (int i = 0; i < a; i++)
       {
         for (int j = 0; j < a; j++)
         {
-          if(matrix2[i][j]!= 0){
-            m.indexColumn[c] = j+1;
-            m.val[c] = matrix2[i][j];
-            c++;
-          }
+          printf("%d ",matrixFromCRS[i][j]);
         }
-        m.indexRow[i] = c;
+        printf("\n");
       }
 
-    printf("\n ---- CSR FORMAT -----");
-    printf("\n Val: ");
+      // Test if both matrix, standard generated one (matrix2) is equal to the generated from CRS format (matrixFromCRS)
 
-    for (int i = 0; i < a*k; i++)
-    {
-      printf("%d ",m.val[i]);
-    }
+      printf("Validating if the standard matrix generated and the one generated from CRS are equals...\n");
 
-    printf("\n col_in: ");
-
-    for (int i = 0; i < a*k; i++)
-    {
-      printf("%d ",m.indexColumn[i]);
-    }
-
-    printf("\n row_ptr: ");
-
-    for (int i = 0; i < a; i++)
-    {
-      printf("%d ",m.indexRow[i]);
-    }
+      for(int i = 0; i<a;i++){
+        for(int j = 0;j<a;j++){
+          if(matrix2[i][j]!=matrixFromCRS[i][j]){ //this element is different
+            printf(" TEST FAILED-------> The matrix are not equals!\n");
+            return 0;
+          }
+        }
+      } printf(" TEST OK-------> The matrix are equals!\n");
+      return 0;
 }
 
 
 /*********** functions ***********/
 
-int find(int array[], int element, int size) {
-
-  int i = 0;
+int find(int array[], int element, int size) { // RETURNS 1 if the element is in the array or 0 if not
   
+  int i = -1;
+
   for (i = 0; i < size; i++)
   {
     if(array[i] == element){
-      return 1; // element is found
+      return 1; // element is found 
     }
   }
   return 0; // is not found
 }
-
